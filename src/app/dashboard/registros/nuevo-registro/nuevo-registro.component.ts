9a92c71ff4c45bf1855293b957c53c60f9ea1a6f
System Administrator <root@MacBook-Air-de-Fernando.local>
2025-08-11 20:06:59 -0600

    ng content -> render fragments

--- src/app/dashboard/registros/nuevo-registro/nuevo-registro.component.ts
+++ src/app/dashboard/registros/nuevo-registro/nuevo-registro.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button.component';
 
 @Component({
   selector: 'app-nuevo-registro',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './nuevo-registro.component.html',
   styleUrl: './nuevo-registro.component.css'
 })
--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
+//selector de elemento <app-button>
+//Selector de atributo button[appButton]
+//Selector de Clase .app-button
+
 @Component({
-  selector: 'app-button',
+  selector: 'button[appButton]', 
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
