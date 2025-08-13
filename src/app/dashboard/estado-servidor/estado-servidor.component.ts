3dfc39571feece4bc91feef0294124d18c74eaf5
Fernando Aquino Terrazas <fernandoaquinoterrazas@MacBook-Air-de-Fernando.local>
2025-08-12 20:32:47 -0600

    Ciclos de vida del comoponente

--- src/app/dashboard/estado-servidor/estado-servidor.component.ts
+++ src/app/dashboard/estado-servidor/estado-servidor.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-estado-servidor',
   standalone: true,
   imports: [],
   templateUrl: './estado-servidor.component.html',
-  styleUrl: './estado-servidor.component.css'
+  styleUrl: './estado-servidor.component.css',
 })
-export class EstadoServidorComponent {
-  estadoActual = 'online';
+export class EstadoServidorComponent implements OnInit, OnChanges {
+  estadoActual: 'online' | 'offline' | 'unknow' = 'online';
+  //hooks de ciclo de vida.
+
+  ngOnInit() {
+    setInterval(() => {
+      const rnd = Math.random();
+      if (rnd > 0.5) {
+        this.estadoActual = 'online';
+      } else if (rnd > 0.1) {
+        this.estadoActual = 'offline';
+      } else {
+        this.estadoActual = 'unknow';
+      }
+    }, 3000);
+  }
+
+  constructor() {}
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error('Method not implemented.');
+  }
 }
