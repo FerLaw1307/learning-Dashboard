a8d8186d9b9b5643df091e7ddb52e8c043dd7481
System Administrator <root@MacBook-Air-de-Fernando.local>
2025-08-12 21:33:19 -0600

    variables de plantillas de html

--- src/app/dashboard/registros/nuevo-registro/nuevo-registro.component.ts
+++ src/app/dashboard/registros/nuevo-registro/nuevo-registro.component.ts
@@ -1,18 +1,17 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
-import { ControlComponent } from "../../../shared/control/control.component";
+import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-nuevo-registro',
   standalone: true,
   imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './nuevo-registro.component.html',
-  styleUrl: './nuevo-registro.component.css'
+  styleUrl: './nuevo-registro.component.css',
 })
 export class NuevoRegistroComponent {
- onSubmit(tituloIngresado: HTMLInputElement, pedidoIngresado: HTMLTextAreaElement){
-  console.log('Enviadooooooooo carnal!');
-  console.dir(tituloIngresado.value);
-  console.dir(pedidoIngresado.value);
- }
+  onSubmit(titulo: string, textoRegistro: string) {
+    console.log(titulo);
+    console.log(textoRegistro);
+  }
 }
